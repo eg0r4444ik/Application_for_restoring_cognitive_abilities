552497a40bf1d92857a5fafee4fd44c6ba6aaed9
egor <egorchik.rog@yandex.ru>
2023-04-05 16:42:41 +0300

    test

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
-import kivy
+import time
+
+import schedule
 import cv2
 import numpy as np
 
-from kivy.app import App
-from kivy.uix.button import Button
-kivy.require('1.0.6') # replace with your current kivy version !
 
-class MyApp(App):
-    green_lower = np.array([25, 52, 72], np.uint8)
-    green_upper = np.array([102, 255, 255], np.uint8)
+# Получение потока с камеры
+cap = cv2.VideoCapture(0)
 
-    # Получение потока с камеры
-    cap = cv2.VideoCapture(0)
+def f(r, g, b):
+    red_filter = [[150, 255], [0, 120], [0, 120]]
+    if not (red_filter[2][0] <= b <= red_filter[2][1]):
+        return False
+    if not (red_filter[1][0] <= g <= red_filter[1][1]):
+        return False
+    if not (red_filter[0][0] <= r <= red_filter[0][1]):
+        return False
+    return True
 
-    while True:
-        # Чтение кадра
-        ret, frame = cap.read()
+while True:
+    # Чтение кадра
+    t1 = time.time()
+    ret, frame = cap.read()
+    for i in range(0, len(frame), 5):
+        for j in range(0, len(frame[i]), 5):
+            cell = frame[i][j]
+            if f(cell[2], cell[1], cell[0]):
+                cell[0] = 255
+                cell[1] = 1
+                cell[2] = 1
+    cv2.imshow('frame', frame)
 
-        # Преобразование цветового пространства в HSV
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Создание маски зеленого цвета
-        mask = cv2.inRange(hsv, green_lower, green_upper)
+    # Отображение кадра с контурами
+    # cv2.imshow('frame', frame)
 
-        # Нахождение контуров
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Выход из цикла при нажатии клавиши 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    print(time.time() - t1)
 
-        # Отображение контуров на кадре
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > 500:
-                cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
+# Остановка потока с камеры и закрытие окон
+cap.release()
+cv2.destroyAllWindows()
 
-        # Отображение кадра с контурами
-        cv2.imshow('frame', frame)
-
-        # Выход из цикла при нажатии клавиши 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Остановка потока с камеры и закрытие окон
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    MyApp().run()
