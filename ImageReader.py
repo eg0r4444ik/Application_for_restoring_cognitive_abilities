325cceb05d2b5127b38c1325d1ff16a248c54370
egor <egorchik.rog@yandex.ru>
2023-05-08 11:12:04 +0300

    find markers zone

--- ImageReader.py
+++ ImageReader.py
@@ -5,8 +5,7 @@ from cv2 import aruco
 import numpy as np
 from threading import Thread
 
-from AreaAnalyzer import AreaAnalyzer
-from ArucoCoordsDetector import ArucoCoordsDetector
+from ColorDetector import ColorDetector
 
 
 class ImageReader(Thread):
@@ -15,8 +14,7 @@ class ImageReader(Thread):
         self.cb = cb
         self.video = cv2.VideoCapture(source_idx)
         self.video.set(cv2.CAP_PROP_BUFFERSIZE, 2)
-        self.analyzer = AreaAnalyzer()
-        self.detector = ArucoCoordsDetector()
+        self.detector = ColorDetector()
         self.running = False
 
     def start(self):
@@ -32,29 +30,6 @@ class ImageReader(Thread):
             _, img = self.video.read()
             hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-            m = cv2.inRange(hsv_img, (0, 0, 0), (255, 255, 255))
-
-            lower_blue = (100, 150, 150)
-            upper_blue = (140, 255, 255)
-            mask3 = cv2.inRange(hsv_img, lower_blue, upper_blue)
-
-            lower_red = np.array([0, 150, 100])
-            upper_red = np.array([10, 255, 255])
-            mask1 = cv2.inRange(hsv_img, lower_red, upper_red)
-
-            lower_red = np.array([170, 150, 100])
-            upper_red = np.array([180, 255, 255])
-            mask2 = cv2.inRange(hsv_img, lower_red, upper_red)
-
-            # Объединение масок
-            mask = mask1 + mask2 + mask3
-
-            # применение маски к изображению
-            result = cv2.bitwise_and(img, img, mask=None)
-
-            blue = cv2.bitwise_and(img, img, mask=mask3)
-            red = cv2.bitwise_and(img, img, mask=(mask1 + mask2))
-
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             dict_aruco = aruco.Dictionary_get(aruco.DICT_5X5_50)
             parameters = aruco.DetectorParameters_create()
@@ -63,20 +38,14 @@ class ImageReader(Thread):
 
             result = aruco.drawDetectedMarkers(img.copy(), corners, ids)
 
-            markers_id = [49]#Номера маркеров, которые хотим найти
+            markers_id = [16]
 
             for marker_id in markers_id:
                 if marker_id in np.ravel(ids):
                     index = np.where(ids == marker_id)[0][0]
-                    print(str(self.detector.center_coords(index, corners)) + " " + str(marker_id) )#Печатаем координаты центра маркеров и их id
-
-
-
+                    print(self.detector.define_zone(ids, corners, index))
 
             self.cb(result)
-            if time.time() - t > 10:
-                self.analyzer.analyze(self.analyzer.detS(red), self.analyzer.detS(blue))
-                t = time.time()
 
         self.video.release()
         cv2.destroyAllWindows()
